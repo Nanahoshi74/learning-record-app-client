069c002abc092ce6de0d1055ae6c41d0a084e704
yuki <yukitake2@icloud.com>
2024-04-28 00:10:30 +0900

    メンテナンス用に変更

---
+++ src/components/maintenance/Maintenance.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Maintenance.css";
+
+const Maintenance = () => {
+  return (
+    <div className="maintenanceWrapper">
+      <span className="maintenancetext">
+        改修工事中です。9月くらいには完成版挙げられるようにしたいです。
+      </span>
+    </div>
+  );
+};
+
+export default Maintenance;
